fa59d5625aead3e572253ddff1cd3353c3ffa408
helena-varela <helena.varelakg@gmail.com>
2025-04-20 22:21:42 +0000

    dishes

--- lib/UI/restaurant/restaurant_screen.dart
+++ lib/UI/restaurant/restaurant_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/UI/_core/app_colors.dart';
 import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/model/dish.dart';
 
 class RestaurantScreen extends StatelessWidget {
   final Restaurant restaurant;
@@ -7,6 +9,49 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(restaurant.name)));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(restaurant.name),
+        titleTextStyle: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+      body: Center(
+        child: Column(
+          spacing: 12.0,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/${restaurant.imagePath}', width: 128),
+            SizedBox(height: 12.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  'Mais pedidos',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.mainColor,
+                  ),
+                ),
+              ),
+            ),
+          Column(
+              children: List.generate(restaurant.dishes.length, (index) {
+              Dish dish = restaurant.dishes[index];
+              return ListTile(
+                leading: Image.asset(
+                'assets/dishes/default.png', 
+                width: 48, 
+                height: 48),
+                title: Text(dish.name),
+                subtitle: Text("R\$${dish.price.toStringAsFixed(2)}"),
+                trailing: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              );
+            }),
+          )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -1,3 +1,4 @@
+import 'dish.dart';
 class Restaurant {
   String id;
   String imagePath;
@@ -6,6 +7,7 @@ class Restaurant {
   double stars;
   int distance;
   List<String> categories;
+  List<Dish> dishes;
 
   Restaurant({
     required this.id,
@@ -15,6 +17,7 @@ class Restaurant {
     required this.stars,
     required this.distance,
     required this.categories,
+    required this.dishes,
   });
 
   Map<String, dynamic> toMap() {
@@ -26,6 +29,7 @@ class Restaurant {
       'stars': stars,
       'distance': distance,
       'categories': categories,
+      'dishes': dishes.map((dish) => dish.toMap()).toList(),
     };
   }
 
@@ -38,6 +42,7 @@ class Restaurant {
       stars: map['stars'],
       distance: map['distance'],
       categories: List<String>.from(map['categories']),
+      dishes: List<Dish>.from(map['dishes'].map((dish) => Dish.fromMap(dish))),
     );
   }
 
